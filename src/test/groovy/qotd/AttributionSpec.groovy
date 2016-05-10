17cd6b0d589480e7345796112efecff61d84ec3d
Mike Goode <goode612@gmail.com>
2016-05-09 22:51:52 -0500

    Commit Attribution tests mods

---
+++ src/test/groovy/qotd/AttributionSpec.groovy
@@ -0,0 +1,35 @@
+package qotd
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Attribution)
+class AttributionSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    def "test for valid attribution name"() {
+        when: ’name is empty’
+        def p = new Attribution(name: ’’)
+        then: ’validation should fail’
+        !p.validate()
+
+        when: ’name is null’
+        def n = new Attribution(name: null)
+        then: ’validation should fail’
+        !n.validate()
+
+        when: ’name is valid’
+        def l = new Attribution(name: ’Mike’)
+        then: ’validation should pass’
+        !l.validate()
+
+    }
+}

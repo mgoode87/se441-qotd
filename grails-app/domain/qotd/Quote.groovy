3085c59c7f6653a72d8680b8eef9e03629086b1a
Mike Goode <goode612@gmail.com>
2016-05-10 17:00:55 -0500

    1

--- grails-app/domain/qotd/Quote.groovy
+++ grails-app/domain/qotd/Quote.groovy
@@ -3,7 +3,7 @@ package qotd
 class Quote {
     String text;
 
-    static belongsTo = [attribution:Attribution];
+    static belongsTo = [attribution: Attribution];
 
     static constraints = {
       text nullable: false
